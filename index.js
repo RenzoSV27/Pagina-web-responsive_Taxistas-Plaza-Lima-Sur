3525328a79a13be20d8e99c655a1ebef3c01de08
Jim Fernandez <n00316076@upn.pe>
2026-06-19 18:54:21 -0500

    correcion css y login

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
     LayoutPublico.inicializar();
+
     if (typeof Animaciones !== 'undefined') {
         Animaciones.landing();
         Animaciones.botonHover();
+    }
+
+    const loginBtn = document.querySelector('.boton-nav-login');
+    const registroBtn = document.querySelector('.boton-nav-registro');
+    const itemPanel = document.getElementById('item-ir-panel');
+
+    if (
+        typeof ServicioAutenticacion !== 'undefined' &&
+        ServicioAutenticacion.estaAutenticado()
+    ) {
+
+        // Ocultar Login
+        if (loginBtn) {
+            loginBtn.closest('li').style.display = 'none';
+        }
+
+        // Ocultar Registro
+        if (registroBtn) {
+            registroBtn.closest('li').style.display = 'none';
+        }
+
+        // Mostrar Panel
+        if (itemPanel) {
+            itemPanel.hidden = false;
+        }
     }
 });
